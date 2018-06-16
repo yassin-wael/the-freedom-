9af8fa7b752301717d5c2b3641c1802daf9f4718
yassin-wael <35656159+yassin-wael@users.noreply.github.com>
2018-06-17 00:43:41 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setGame("in development");
+});
+
+//THIS MUST BE THIS WAY
+bot.login(process.env.BOT_TOKEN);
